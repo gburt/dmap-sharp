37bc1b01cc443303c5e1b44fbd83b49b41ef89d9
James Willcox <jwilcox@mono-cvs.ximian.com>
2005-11-20 04:10:54 +0000

    mv configure.in configure.ac
    svn path=/trunk/daap-sharp/; revision=53284

--- autogen.sh
+++ autogen.sh
@@ -17,7 +17,7 @@ cd $srcdir
 PROJECT=daap-sharp
 TEST_TYPE=-f
 FILE=src/Client.cs
-CONFIGURE=configure.in
+CONFIGURE=configure.ac
 aclocalinclude="-I . $ACLOCAL_FLAGS"
 
 DIE=0
@@ -38,7 +38,7 @@ DIE=0
         DIE=1
 }
 
-(grep "^AM_PROG_LIBTOOL" configure.in >/dev/null) && {
+(grep "^AM_PROG_LIBTOOL" configure.ac >/dev/null) && {
   ($LIBTOOL --version) < /dev/null > /dev/null 2>&1 || {
     echo
     echo "**Error**: You must have \`libtool' installed to compile $PROJECT."
@@ -66,7 +66,7 @@ case $CC in
 *xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
 esac
 
-(grep "^AM_PROG_LIBTOOL" configure.in >/dev/null) && {
+(grep "^AM_PROG_LIBTOOL" configure.ac >/dev/null) && {
     echo "Running $LIBTOOLIZE ..."
     $LIBTOOLIZE --force --copy
 }
